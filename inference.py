a24a91ddff3d41d9efa99f397e333370d7167569
Han Xiao <han.xiao@aalto.fi>
2017-09-23 19:49:59 +0000

    inference can take top-k steiner trees for samples

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ from minimum_steiner_tree import min_steiner_tree
 from core import sample_steiner_trees, node_occurrence_freq
 from graph_helpers import extract_nodes
 
+
 def infer_infected_nodes(g, obs, method="min_steiner_tree", **kwargs):
     """besides observed infections, infer other infected nodes
     if method is 'sampling', then `kwargs` has 3 cases:
@@ -18,15 +19,20 @@ def infer_infected_nodes(g, obs, method="min_steiner_tree", **kwargs):
         st = min_steiner_tree(g, obs)
         remain_infs = set(map(int, st.vertices()))
         return remain_infs
-    else:
+    else:  # sampling
+        subset_size = kwargs.get('subset_size', None)
         if 'st_trees' in kwargs:
             st_trees = kwargs['st_trees']
         elif 'sp_trees' in kwargs:
             sp_trees = kwargs['sp_trees']
-            st_trees = sample_steiner_trees(g, obs, sp_trees=sp_trees)
+            st_trees = sample_steiner_trees(g, obs,
+                                            subset_size=subset_size,
+                                            sp_trees=sp_trees)
         else:
             n_samples = kwargs['n_samples']
-            st_trees = sample_steiner_trees(g, obs, n_samples, sp_trees=None)
+            st_trees = sample_steiner_trees(g, obs, n_samples,
+                                            subset_size=subset_size,
+                                            sp_trees=None)
 
         min_inf_proba = kwargs.get('min_inf_proba', 0.5)
         inf_probas = np.array([node_occurrence_freq(n, st_trees)[0]
--- test_inference.py
+++ test_inference.py
@@ -11,6 +11,8 @@ def test_infer_infected_nodes_sampling_approach(g, obs):
     # with steiner trees
     st_trees = sample_steiner_trees(g, obs, n_samples, sp_trees=sp_trees)
     inf_nodes = infer_infected_nodes(g, obs, method="sampling", st_trees=st_trees)
+
+    # simple test, just make sure observation is in the prediction
     assert set(obs).issubset(set(inf_nodes))
 
     # with spanning trees
@@ -21,3 +23,9 @@ def test_infer_infected_nodes_sampling_approach(g, obs):
     # without anything
     inf_nodes2 = infer_infected_nodes(g, obs, method="sampling", n_samples=n_samples)
     assert set(obs).issubset(set(inf_nodes2))
+
+    # take top-25 trees for inference
+    inf_nodes3 = infer_infected_nodes(g, obs, method="sampling",
+                                      subset_size=25,
+                                      n_samples=n_samples)
+    assert set(obs).issubset(set(inf_nodes3))
