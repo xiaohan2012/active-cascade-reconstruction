215e06de406a9fede9c3ea46a2f4f5d91c585c18
Han Xiao <han.xiao@aalto.fi>
2017-09-24 19:29:43 +0000

    inference gives probability now

--- inference.py
+++ inference.py
@@ -4,14 +4,37 @@ from core import sample_steiner_trees, node_occurrence_freq
 from graph_helpers import extract_nodes
 
 
-def infer_infected_nodes(g, obs, method="min_steiner_tree", **kwargs):
-    """besides observed infections, infer other infected nodes
-    if method is 'sampling', then `kwargs` has 3 cases:
+def infection_probability(g, obs, **kwargs):
+    """
+    `kwargs` has 3 cases:
 
     1. `st_trees`: steiner tree samples, so no need to sample them
     2. `sp_trees`: spanning tree samples, need to extract steiner trees
     3. no trees: then we sample `n_samples` steiner trees
-    
+    """
+       
+    subset_size = kwargs.get('subset_size', None)
+    if 'st_trees' in kwargs:
+        st_trees = kwargs['st_trees']
+    elif 'sp_trees' in kwargs:
+        sp_trees = kwargs['sp_trees']
+        st_trees = sample_steiner_trees(g, obs,
+                                        subset_size=subset_size,
+                                        sp_trees=sp_trees)
+    else:
+        n_samples = kwargs['n_samples']
+        st_trees = sample_steiner_trees(g, obs, n_samples,
+                                        subset_size=subset_size,
+                                        sp_trees=None)
+    inf_probas = np.array([node_occurrence_freq(n, st_trees)[0]
+                           for n in extract_nodes(g)]) / len(st_trees)
+    return inf_probas
+
+
+def infer_infected_nodes(g, obs, method="min_steiner_tree", **kwargs):
+    """besides observed infections, infer other infected nodes
+    if method is 'sampling', refer to infection_probability,
+
     `min_inf_proba` is the minimum infection probability to be considered "'infected'
     """
     assert method in {"min_steiner_tree", "sampling"}
@@ -20,21 +43,6 @@ def infer_infected_nodes(g, obs, method="min_steiner_tree", **kwargs):
         remain_infs = set(map(int, st.vertices()))
         return remain_infs
     else:  # sampling
-        subset_size = kwargs.get('subset_size', None)
-        if 'st_trees' in kwargs:
-            st_trees = kwargs['st_trees']
-        elif 'sp_trees' in kwargs:
-            sp_trees = kwargs['sp_trees']
-            st_trees = sample_steiner_trees(g, obs,
-                                            subset_size=subset_size,
-                                            sp_trees=sp_trees)
-        else:
-            n_samples = kwargs['n_samples']
-            st_trees = sample_steiner_trees(g, obs, n_samples,
-                                            subset_size=subset_size,
-                                            sp_trees=None)
         min_inf_proba = kwargs.get('min_inf_proba', 0.5)
-        inf_probas = np.array([node_occurrence_freq(n, st_trees)[0]
-                               for n in extract_nodes(g)]) / len(st_trees)
-
+        inf_probas = infection_probability(g, obs, **kwargs)
         return (inf_probas >= min_inf_proba).nonzero()[0]
