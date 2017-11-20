2f231d69d64303b3134f21d6fe9b4a77af062a22
Han Xiao <han.xiao@aalto.fi>
2017-11-20 14:29:54 +0000

    type in cmd args

--- simulate_cascades.py
+++ simulate_cascades.py
@@ -6,12 +6,17 @@ from experiment import gen_input
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('-g', '--graph', help='graph name')
-parser.add_argument('-n', '--n_cascades', default=100, help='number of cascades')
-parser.add_argument('-s', '--stop_fraction', default=0.5, help='fraction of infected nodes to stop')
-parser.add_argument('-o', '--obs_fraction', default=0.2, help='fraction of observed  nodes')
-parser.add_argument('-p', '--infection_proba', default=0.5, help='infection probability')
-parser.add_argument('-d', '--output_dir', default='cascade', help='output directory')
+parser.add_argument('-g', '--graph', required=True, help='graph name')
+parser.add_argument('-n', '--n_cascades', type=int, default=100,
+                    help='number of cascades')
+parser.add_argument('-s', '--stop_fraction', type=float, default=0.5,
+                    help='fraction of infected nodes to stop')
+parser.add_argument('-o', '--obs_fraction', type=float, default=0.2,
+                    help='fraction of observed  nodes')
+parser.add_argument('-p', '--infection_proba', type=float, default=0.5,
+                    help='infection probability')
+parser.add_argument('-d', '--output_dir', default='cascade',
+                    help='output directory')
 
 args = parser.parse_args()
 graph_name = args.graph
@@ -23,7 +28,8 @@ for i in tqdm(range(args.n_cascades)):
                        q=args.obs_fraction,
                        p=args.infection_proba)
 
-    d = os.path.join(args.output_dir, graph_name)
+    # d = os.path.join(args.output_dir, graph_name)
+    d = args.output_dir
     if not os.path.exists(d):
         os.makedirs(d)
     path = os.path.join(d, '{}.pkl'.format(i))
