830ff697236827d2268c107937b1d44a712b2bcb
Han Xiao <han.xiao@aalto.fi>
2018-01-12 12:04:26 +0000

    tree stat with prediction error and query score

--- test_tree_stat.py
+++ test_tree_stat.py
@@ -2,6 +2,8 @@ import pytest
 import numpy as np
 from numpy.testing import assert_array_equal as assert_eq_np
 from graph_tool import Graph
+from scipy.stats import entropy
+
 from tree_stat import TreeBasedStatistics
 
 
@@ -31,8 +33,12 @@ def g():
     return g
 
 
-def test_count_and_proba(g, trees):
-    stat = TreeBasedStatistics(g, trees)
+@pytest.fixture
+def stat(g, trees):
+    return TreeBasedStatistics(g, trees)
+
+
+def test_count_and_proba(stat, trees):
     n_trees = len(trees)
     targets = list(range(1, 6))
     query = 0
@@ -46,13 +52,12 @@ def test_count_and_proba(g, trees):
                  arr_c1)
 
     assert_eq_np(stat.proba(query, condition=0, targets=targets),
-                 arr_c0 / n_trees)
+                 arr_c0 / 2)
     assert_eq_np(stat.proba(query, condition=1, targets=targets),
-                 arr_c1 / n_trees)
+                 arr_c1 / 3)
 
 
-def test_update_trees(g, trees, new_trees):
-    stat = TreeBasedStatistics(g, trees)
+def test_update_trees(stat, new_trees):
     stat.update_trees(new_trees, query=0, state=1)
 
     query = 1
@@ -66,8 +71,34 @@ def test_update_trees(g, trees, new_trees):
                  arr_c1)
 
 
-def test_update_trees_insufficient_trees(g, trees, new_trees):
-    stat = TreeBasedStatistics(g, trees)
+def test_update_trees_insufficient_trees(stat, new_trees):
     with pytest.raises(AssertionError):
         # insufficient length
         stat.update_trees(new_trees[:1], query=0, state=1)
+
+
+@pytest.fixture
+def trees1():
+    return [
+        {0, 1, 2},
+        {1, 2},
+        {1, 2, 3},
+        {1, 2, 3, 4},
+    ]
+
+
+@pytest.fixture
+def stat1(g, trees1):
+    return TreeBasedStatistics(g, trees1)
+
+
+def test_prediction_error(stat1):
+    actual = stat1.prediction_error(0, 0, [3, 4])
+    expected = entropy([1/3, 2/3]) * 2
+    assert actual == expected
+
+
+def test_query_score(stat1):
+    actual = stat1.query_score(0, [3, 4])
+    expected = entropy([1/3, 2/3]) * 2 * 3/4  # + error = 0 for state=1
+    assert actual == expected
