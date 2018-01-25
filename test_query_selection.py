daaa119ef912e23e34a38632e14c559de7252633
Han Xiao <han.xiao@aalto.fi>
2018-01-25 20:34:44 +0000

    test for two approximation schemes

--- test_query_selection.py
+++ test_query_selection.py
@@ -63,7 +63,7 @@ def test_query_method(g, query_method, sampling_method, root_sampler):
         check_error_esitmator(qs, aux['c'], error_estimator)
 
 
-def test_no_more_query(g):    
+def test_no_more_query(g):
     gv = remove_filters(g)
 
     q_gen = RandomQueryGenerator(gv)
@@ -71,3 +71,51 @@ def test_no_more_query(g):
 
     qs, aux = sim.run(g.num_vertices()+100)
     assert len(qs) < g.num_vertices()
+
+
+def build_simulator_using_prediction_error_query_selector(g, **kwargs):
+    gv = remove_filters(g)
+    gi = from_gt(g)
+    pool = TreeSamplePool(gv,
+                          n_samples=1000,
+                          method='loop_erased',
+                          gi=gi,
+                          return_tree_nodes=True  # using tree nodes
+    )
+
+    q_gen = PredictionErrorQueryGenerator(gv, pool,
+                                          error_estimator=TreeBasedStatistics(gv),
+                                          root_sampler=None,
+                                          **kwargs)
+    return Simulator(gv, q_gen, gi=gi, print_log=True), q_gen
+
+
+@pytest.mark.parametrize("repeat_id", range(5))
+def test_prediction_error_with_candidate_pruning(g, repeat_id):
+    min_probas = [0, 0.1, 0.2, 0.3, 0.4]
+    cand_nums = []
+
+    for min_proba in min_probas:
+        sim, q_gen = build_simulator_using_prediction_error_query_selector(
+            g, prune_nodes=True, min_proba=min_proba)
+        
+        sim.run(0)  # just get the candidates
+        q_gen.prune_candidates()  # and prune the candidates
+
+        cand_nums.append(len(q_gen._cand_pool))
+
+    # number of candidates should be decreasing (more accurately, non-increasing)
+    for prev, cur in zip(cand_nums, cand_nums[1:]):
+        assert prev > cur
+
+
+def test_prediction_error_sample_nodes_for_estimation(g):
+    n_node_samples_list = [10, 20, 30, 40, 50]
+    for n_node_samples in n_node_samples_list:
+        sim, q_gen = build_simulator_using_prediction_error_query_selector(
+            g, n_node_samples=n_node_samples)
+        sim.run(0)
+
+        samples = q_gen._sample_nodes_for_estimation()
+        assert len(samples) == n_node_samples
+    
