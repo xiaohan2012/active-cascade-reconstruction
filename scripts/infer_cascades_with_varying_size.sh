26450fbec3d06b8616df464b849458e4df0584e9
Han Xiao <han.xiao@aalto.fi>
2018-01-28 10:43:20 +0000

    inference shell script

---
+++ scripts/infer_cascades_with_varying_size.sh
@@ -0,0 +1,26 @@
+#! /bin/zsh
+
+graph="grqc"
+sample_method=loop_erased
+
+inf_method="inf_probas"
+
+cascade_model="si"
+stop_fractions=(0.01 0.02 0.04 0.08 0.16 0.32)
+obs_fraction=0.1
+
+for stop_fraction in ${stop_fractions}; do
+    dataset_id="${graph}-m${cascade_model}-s${stop_fraction}-o${obs_fraction}"
+
+    cascade_dir="cascade/${dataset_id}"
+    print "on ${dataset_id}"
+    python3 infer_from_queries.py \
+	    -g ${graph} \
+	    -s 250 \
+	    -m ${inf_method} \
+	    -c ${cascade_dir} \
+	    -q outputs/queries/${dataset_id}/${sample_method} \
+	    -p outputs/${inf_method}/${dataset_id}/${sample_method}
+	
+    done
+done
