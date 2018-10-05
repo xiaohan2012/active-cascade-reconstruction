bf10f270886882417bf04140b2f7f211de6a9f3f
Han Xiao <han.xiao@aalto.fi>
2018-10-05 21:37:56 +0300

    test query and inference using singularity on triton

--- scripts/test_query.sh
+++ scripts/test_query.sh
@@ -1,17 +1,23 @@
 #! /bin/zsh
 
-kernprof -l generate_queries.py \
-	 -g lattice-100 \
-	 -f ' ' \
-	 -q entropy \
-	 -n 3 \
-	 -s 100 \
-	 -p 0.00 \
-	 -m simulation \
-	 -r true_root \
-	 -c cascade-weighted/lattice-100-msi-s0.25-o0.25-omuniform \
-	 -d outputs/queries/test/ \
-	 --verbose --debug \
-	 --infection_proba 0.5 \
-	 --cascade_size 0.5 \ 
-	 --cascade_model "si"
+DATA_DIR=/experiment
+
+if [ -d $DATA_DIR/outputs/queries/test/ ]; then
+    rm -rf $DATA_DIR/outputs/queries/test/
+fi
+
+python3 generate_queries.py \
+	-g infectious \
+	-f '_0.5' \
+	-q entropy \
+	-n 3 \
+	-s 100 \
+	-p 0.00 \
+	-m simulation \
+	-r true_root \
+	-c $DATA_DIR/cascades/infectious-msi-s0.1-o0.1-omuniform \
+	-d $DATA_DIR/outputs/queries/test/ \
+	--verbose --debug \
+	--infection_proba 0.5 \
+	--cascade_size 0.1 \
+	--cascade_model "si"
