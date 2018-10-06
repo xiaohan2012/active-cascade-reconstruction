c618be2b46c29a30a9c2d77ca5bb7d7726725d14
Han Xiao <han.xiao@aalto.fi>
2018-10-06 17:33:07 +0300

    sbatch script

---
+++ scripts/sbatch_experiment.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+#SBATCH --job-name=email-univ
+#SBATCH --output=/scratch/work/xiaoh1/data-active-cascade-reconstruction/logs/email-univ
+#SBATCH --cpus-per-task 1 
+#SBATCH --time 01:00:00  # per task?
+#SBATCH --mem=1G
+#SBATCH --array=1-384 # 96 rounds x 4 strategies
+
+GRAPH=email-univ  # change this for new graphs
+
+query_args_file=exp_args/query_${GRAPH}.txt
+infer_args_file=exp_args/infer_${GRAPH}.txt
+
+n=$SLURM_ARRAY_TASK_ID
+
+query_args=`sed "${n}q;d" ${query_args_file}`
+infer_args=`sed "${n}q;d" ${infer_args_file}`
+
+eval "./singularity/exec.sh python3 query_one_round.py ${query_args}"
+eval "./singularity/exec.sh python3 infer_one_round.py ${infer_args}"
